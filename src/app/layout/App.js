7bea51b2e488f2d40d0165a4cfd1b4d88d6fd46c
AbzG97 <abdulla.ghazalah@hotmail.com>
2021-03-16 09:27:54 -0400

    init commit'

---
+++ src/app/layout/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Re-vents</h1>
+    </div>
+  );
+}
+
+export default App;
